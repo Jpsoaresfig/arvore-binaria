7bf459c1a1a8abbce55a239516704ffcabeef734
Jpsoaresfig <jpbusinesss@hotmail.com>
2023-06-06 20:25:19 -0300

    Revert "criação da funcao busca"
    This reverts commit 6b9b99423517d01b2f1ee45db42ea813e636870d.

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -5,6 +5,7 @@ import entities.Student;
 public class BinaryTree {
     private Node root;
 
+
     public BinaryTree() {
     }
 
@@ -16,67 +17,60 @@ public class BinaryTree {
         this.root = root;
     }
 
-    public void insert(Student student) {
+    public void insert(Student student){
         
     }
 
-    private void insert(Node node, Student student) {
+    private void insert(Node node, Student student){
         
     }
 
-    public void remove(Long rgm) {
+    public void remove(Long rgm){
         
     }
 
-    private void remove(Node node, Long rgm) {
+    private void remove(Node node, Long rgm){
+        
+    }
+
+    public Student search(Long rgm){
+        return null;
+    }
+
+    private Student search(Node node, Long rgm){
+        return null;
+    }
+
+    public void emptyTree(){
+        
+    }
+
+    private void emptyTree(Node node){
+        
+    }
+
+    public void printTreeInOrder(){
+        
+    }
+
+    private void printTreeInOrder(Node node){
+        
+    }
+
+    public void printTreePreOrder(){
+        
+    }
+
+    private void printTreePreOrder(Node node){
+        
+    }
+
+    public void printTreePostOrder(){
+        
+    }
+
+    private void printTreePostOrder(Node node){
         
     }
     
-    public Student search(Long rgm) { // recebe um RGm e retorna o student
-        return search(root, rgm);
-    }
-    
-    private Student search(Node node, Long rgm) {
-        if (node == null || node.getStudent().getRgm().equals(rgm)) { // verifica duas condições: se o nó é nulo ou se encontrou o RGm
-            return node != null ? node.getStudent() : null; // retorna o estudante caso encontrado, caso contrário retorna null
-        }
-    
-        if (rgm < node.getStudent().getRgm()) {
-            return search(node.getLeft(), rgm); // continua a busca no lado esquerdo da árvore
-        } else {
-            return search(node.getRight(), rgm); // continua a busca no lado direito da árvore
-        }
-    }
-    
-    public void emptyTree() {
-        
-    }
-
-    private void emptyTree(Node node) {
-        
-    }
-
-    public void printTreeInOrder() {
-        
-    }
-
-    private void printTreeInOrder(Node node) {
-        
-    }
-
-    public void printTreePreOrder() {
-        
-    }
-
-    private void printTreePreOrder(Node node) {
-        
-    }
-
-    public void printTreePostOrder() {
-        
-    }
-
-    private void printTreePostOrder(Node node) {
-        
-    }
 }
