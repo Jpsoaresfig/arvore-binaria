5b30b0883dc21e482f22f5cee5fdcd1d0292c1c9
Luiz Filipe <luizfilipemr17@gmail.com>
2023-06-08 15:11:03 -0300

    Base Menu

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,20 @@
 package app;
 
+import java.util.Scanner;
+
+import tree.BinaryTree;
+import tree.ReadFile;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        ReadFile.read(tree);
+
+        UI.printHeader();
+
+        UI.printOptions(sc, tree);
+        
+        sc.close(); 
     }
 }
--- src/app/UI.java
+++ src/app/UI.java
@@ -1,7 +1,98 @@
 package app;
 
-public class UI {
-    public void menu(){
+import java.util.Scanner;
 
+import tree.BinaryTree;
+
+public class UI {
+    public static void printHeader(){
+        System.out.println("Equipe:");
+        System.out.println("João Pedro Soares");
+        System.out.println("Luiz Filipe de Matos Ramos");
+        System.out.println("Maria Vitória Berto");
+        System.out.println("Rafael Benitez");
+        System.out.println();
+        System.out.println("Professor: Leandro Santana");
+        System.out.println("Disciplina: Estrutura de Dados I");
+    }
+
+    public static void printOptions(Scanner sc, BinaryTree tree){
+        System.out.println("             Árvore Binária");
+
+        System.out.println("""
+            1 - INSERIR - fornecer RGM e Nome
+            2 - REMOVER UM NÓ - fornecer o RGM a remover
+            3 - PESQUISAR - fornecer o RGM a pesquisar
+            4 - ESVAZIAR A ÁRVORE
+            5 - EXIBIR A ÁRVORE - três opções: PRÉ, IN ou PÓS
+            0 - SAIR
+                """);
+        System.out.println();
+        System.out.print("Opção: ");
+        int op = sc.nextInt();
+        sc.nextLine();
+        executeOptions(op, sc, tree);
+    }
+
+
+    public static void executeOptions(int op, Scanner sc, BinaryTree tree){
+        switch (op) {
+            case 1:
+                insert(sc, tree);
+                break;
+            
+            case 2:
+                remove(sc, tree);
+                break;
+            case 3:
+                search(sc, tree);
+            break;
+            case 4:
+                emptyTree(sc, tree);
+                break;
+            case 5:
+                show(sc, tree);
+                break;
+            
+            default:
+                System.out.println("Opção Inválida!");
+                confirmBackToMenu(sc, tree);
+                break;
+            case 0:
+                System.out.println("Saindo... Obrigado por usar nosso programa!");
+                break;
+        }
+    }
+
+    private static void insert(Scanner sc, BinaryTree tree) {
+
+        confirmBackToMenu(sc, tree);
+    }
+
+    private static void remove(Scanner sc, BinaryTree tree) {
+        
+        confirmBackToMenu(sc, tree);
+    }
+    
+    private static void search(Scanner sc, BinaryTree tree) {
+        
+        confirmBackToMenu(sc, tree);
+    }
+
+    
+    private static void emptyTree(Scanner sc, BinaryTree tree) {
+
+        confirmBackToMenu(sc, tree);
+    }
+
+    private static void show(Scanner sc, BinaryTree tree) {
+
+        confirmBackToMenu(sc, tree);
+    }
+
+    private static void confirmBackToMenu(Scanner sc, BinaryTree tree){
+        System.out.println("Pressione ENTER para voltar ao menu");
+        sc.nextLine();
+        printOptions(sc, tree);
     }
 }
